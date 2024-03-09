40d66fd227e3471e5000b33b9da6f9c3b138c491
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-09 21:31:14 +0200

    Atualizações

--- painel/site/dashboard/index.php
+++ painel/site/dashboard/index.php
@@ -30,7 +30,7 @@
 <div class="col p-3">
     <div class="alert alert-primary" style="height:140px;">
         <div class="d-flex justify-content-between">
-            <div>
+            <div class="flex-fill">
                 <b><?=$titulo?></b>
                 <h2><?=$total?></h2>        
             </div>
