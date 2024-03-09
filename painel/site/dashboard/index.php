635c6d3b21e1ebfbd2f11b1bdabbc1f74c5dfc36
tamerelmenoufi <tamer@mohatron.com.br>
2024-03-09 10:34:36 -0400

    Atualizações

--- painel/site/dashboard/index.php
+++ painel/site/dashboard/index.php
@@ -5,11 +5,19 @@ Relatórios
 <div class="row g-0">
     <div class="m-3"></div>
 <?php
-    for($i=0;$i<12;$i++){
+
+    $placas = [
+        'banners' => 'Nammers',
+        'servicos' => 'Produtos',
+        'time' => 'Time da empresa',
+        'depoimentos' => 'Depoimentos',
+    ];
+    
+    foreach($placas as $tabela => $titulo){
 ?> 
 <div class="col p-3">
     <div class="alert alert-primary" style="height:90px;">
-        Dados teste
+        <h2><?=$titulo?></h2>
     </div>
 </div>
 <?php
