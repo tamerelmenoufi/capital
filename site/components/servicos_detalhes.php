830bf1f986b4d2680f628819736ef0015473df84
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-09 13:58:43 +0200

    Atualizações

--- site/components/servicos_detalhes.php
+++ site/components/servicos_detalhes.php
@@ -23,7 +23,7 @@
       <div class="container">
 
         <div class="d-flex justify-content-between align-items-center">
-          <h2 style=""><?=$d->titulo?>XXX</h2>
+          <h2 style=""><?=$d->titulo?></h2>
 
         </div>
 
