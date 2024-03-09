0a47eb5da3759bac5781897a1ad0ca3daf5bf071
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-09 21:25:03 +0200

    Atualizações

--- painel/site/dashboard/index.php
+++ painel/site/dashboard/index.php
@@ -35,7 +35,7 @@
                 <h2><?=$total?></h2>        
             </div>
             <div class="col-6">
-                <canvas class="grafico" height="100"></canvas>        
+                <canvas class="grafico" height="100" style="margin-bottom:10px;"></canvas>        
             </div>
         </div>
     </div>
