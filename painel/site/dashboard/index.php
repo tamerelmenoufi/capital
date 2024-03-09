af07ca873fdb93540f1dd94532ac0d847d75c2cd
tamerelmenoufi <tamer@mohatron.com.br>
2024-03-09 10:22:13 -0400

    Atualizações

--- painel/site/dashboard/index.php
+++ painel/site/dashboard/index.php
@@ -1,7 +1,19 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/painel/lib/includes.php");
-
 ?>
+<div class="row">
+<?php
+    for($i=10;$i<12;$i++){
+?> 
+<div class="col">
+    <div class="alert alert-primary" style="height:60px;">
+        Dados teste
+    </div>
+</div>
+<?php
+    }
+?>
+</div>
 <script>
     $(function(){
 
