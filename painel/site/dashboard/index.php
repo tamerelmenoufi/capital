07991accabb2e4d9ba1159b68c6664a510261630
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-09 19:13:44 +0200

    Atualizações

--- painel/site/dashboard/index.php
+++ painel/site/dashboard/index.php
@@ -40,17 +40,13 @@
 
         Carregando('none');
 
-        const Utils;
-        const DATA_COUNT = 5;
-        const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
-
         const data = {
         labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
         datasets: [
             {
             label: 'Dataset 1',
-            data: Utils.numbers(NUMBER_CFG),
-            backgroundColor: Object.values(Utils.CHART_COLORS),
+            data: [10,20,24,11,33],
+            backgroundColor: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
             }
         ]
         };
