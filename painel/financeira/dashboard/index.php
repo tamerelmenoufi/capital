b0d091e87d5f665e089adcc264814b000065afa0
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-12 16:37:29 +0200

    Atualizações

--- painel/financeira/dashboard/index.php
+++ painel/financeira/dashboard/index.php
@@ -3,11 +3,8 @@
 
 ?>
 
-<h3>Sistema Capital Financeira</h3>
-
-
-<div class="card">
-  <h5 class="card-header">Featured</h5>
+<div class="card m-3">
+  <h5 class="card-header">Sistema Capital Financeira</h5>
   <div class="card-body">
     <h5 class="card-title">Special title treatment</h5>
     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
