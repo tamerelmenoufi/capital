ea4f9c1c4c6a59bf8f705599a1b2d56c3f277fa2
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-12 16:41:54 +0200

    Atualizações

--- painel/financeira/componentes/menu_lateral/menu.php
+++ painel/financeira/componentes/menu_lateral/menu.php
@@ -31,6 +31,14 @@
       </div>
     </div>
 
+    <div class="row mb-1 menu-cinza">
+      <div class="col">
+        <a url="financeira/vctex/index.php" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
+          <i class="fa-solid fa-chart-line"></i> VCTEX
+        </a>
+      </div>
+    </div>
+
 
   </div>
 </div>
---
+++ painel/financeira/vctex/index.php
@@ -0,0 +1,23 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/painel/lib/includes.php");
+
+?>
+
+<div class="card m-3">
+  <h5 class="card-header">Sistema Capital Financeira - VCTEX</h5>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
+<script>
+    $(function(){
+
+        Carregando('none');
+
+
+    })
+</script>
