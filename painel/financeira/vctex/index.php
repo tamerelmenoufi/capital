955b7438c1ca973395baeb0cd0fc493641faaac3
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-12 16:47:58 +0200

    Atualizações

--- painel/financeira/vctex/index.php
+++ painel/financeira/vctex/index.php
@@ -1,14 +1,20 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/painel/lib/includes.php");
 
+$query = "select * from configuracoes where codigo = '1'";
+$result = mysqli_query($con, $query);
+$d = mysqli_fetch_object($result);
+
+$tabelas = json_decode($d->api_tabelas);
+
 ?>
 
 <div class="card m-3">
   <h5 class="card-header">Sistema Capital Financeira - VCTEX</h5>
   <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">Tabelas disponíveis</h5>
+    <p class="card-text"><?=var_dump($tabelas)?></p>
+    <button atualiza class="btn btn-primary">Atualizar</button>
   </div>
 </div>
 
@@ -17,6 +23,16 @@
     $(function(){
 
         Carregando('none');
+
+        $("button[atualiza]").click(function(){
+            Carregando();
+            $.ajax({
+                url:"financeira/vctex/index.php",
+                success:function(dados){
+                    $("#paginaHome").html(dados);
+                }
+            })
+        })
 
 
     })
