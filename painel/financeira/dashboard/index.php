3d274a415d63bb20ee7dd683f79172d3a9b18b11
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-12 16:33:23 +0200

    Atualizações

---
+++ painel/financeira/dashboard/index.php
@@ -0,0 +1,14 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/painel/lib/includes.php");
+
+?>
+
+<h3>Sistema Capital Financeira</h3>
+<script>
+    $(function(){
+
+        Carregando('none');
+
+
+    })
+</script>
