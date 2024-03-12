7798700e0769fe5943b9313d0e4e94423d9d1746
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-12 14:04:53 +0200

    Atualizações

--- api/teste.php
+++ api/teste.php
@@ -8,6 +8,6 @@
 
     $token = "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjRjOWZhODZlLWY3ZmYtNDJjNC04N2FmLWI5NjExYjAyZDVjYSIsImlhdCI6MTcwOTkxMzI0NywiaXNzIjoidmN0ZXhfdGVzdCIsImF1ZCI6InZjdGV4X3Rlc3QiLCJleHAiOjE3MDk5MjA0NDd9.k6jWn5akclGnSzTYM75wx8hM2354g6A3d-gxkrTKjn8";
 
-    // echo $retorno = $vctex->Tabelas($token);
+    echo $retorno = $vctex->Tabelas($token);
     // echo $retorno = $vctex->Simular($token);
-    echo $retorno = $vctex->Credito($token);
+    // echo $retorno = $vctex->Credito($token);
