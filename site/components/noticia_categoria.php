2aca3c24cc765fc1ac9e289f7c27aa3ab37c1223
livia128924 <livia128924@gmail.com>
2024-03-13 09:36:53 -0300

    atualizando

--- site/components/noticia_categoria.php
+++ site/components/noticia_categoria.php
@@ -80,7 +80,7 @@
       <div class="container">
 
         <div class="d-flex justify-content-between align-items-center">
-          <h2 style="">Noticias</h2>
+          <h2 style="">Todas as Noticias</h2>
 
         </div>
 
