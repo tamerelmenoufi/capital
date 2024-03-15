7294d28ddcf947cb48b4243b018a99c96a90c108
livia128924 <livia128924@gmail.com>
2024-03-15 11:27:03 -0300

    atualizando

--- site/components/time.php
+++ site/components/time.php
@@ -104,18 +104,21 @@
 
 
 
-    <div class="your-class">
+    
+  <div class="your-class">
     <div>your content</div>
     <div>your content</div>
     <div>your content</div>
   </div>
 
+  <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <script type="text/javascript" src="slick/slick.min.js"></script>
 
-    <script type="text/javascript">
+  <script type="text/javascript">
     $(document).ready(function(){
       $('.your-class').slick({
         setting-name: setting-value
       });
     });
   </script>
-    
