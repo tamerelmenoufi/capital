8f736f19ac0663a92acaed7290ee34fa9e5edc73
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-16 16:53:32 +0200

    Atualizações

--- painel/financeira/vctex/consulta.php
+++ painel/financeira/vctex/consulta.php
@@ -396,15 +396,20 @@
                     </tr>
                 </tbody> 
             </table>
-            <div class="input-group m-3">
-                <span class="input-group-text"><i class="fa-solid fa-link"></i></span>
-                <div class="form-control"><?=$proposta->data->formalizationLink?></div>
-                <button class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa-solid fa-copy"></i></button>
-                <button class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa-brands fa-whatsapp"></i></button>
-                <button class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa-solid fa-comment-sms"></i></button>
-                <button class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa-solid fa-at"></i></button>
-                <button class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa-solid fa-rotate"></i></button>
+            <div class="row">
+                <div class="row">
+                    <div class="input-group">
+                        <span class="input-group-text"><i class="fa-solid fa-link"></i></span>
+                        <div class="form-control"><?=$proposta->data->formalizationLink?></div>
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa-solid fa-copy"></i></button>
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa-brands fa-whatsapp"></i></button>
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa-solid fa-comment-sms"></i></button>
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa-solid fa-at"></i></button>
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa-solid fa-rotate"></i></button>
+                    </div>                    
+                </div>
             </div>
+
             
             
             <?php
