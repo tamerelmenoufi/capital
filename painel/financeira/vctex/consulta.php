881f784a6c277202eabbb81c3b8691e1a84ae84d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-16 16:55:45 +0200

    Atualizações

--- painel/financeira/vctex/consulta.php
+++ painel/financeira/vctex/consulta.php
@@ -397,8 +397,8 @@
                 </tbody> 
             </table>
             <div class="row">
-                <div class="col">
-                    <div class="input-group m-2">
+                <div class="col p-2">
+                    <div class="input-group">
                         <span class="input-group-text"><i class="fa-solid fa-link"></i></span>
                         <div class="form-control"><?=$proposta->data->formalizationLink?></div>
                         <button class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa-solid fa-copy"></i></button>
