46a41b6c8269f76a3cd436bd3fa7e4955fc1fc95
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-16 17:00:25 +0200

    Atualizações

--- painel/financeira/vctex/consulta.php
+++ painel/financeira/vctex/consulta.php
@@ -461,6 +461,11 @@
 
         Carregando('none');
 
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+        })
+
         $("input[busca]").mask("999.999.999-99");
 
         $("button[atualiza]").click(function(){
