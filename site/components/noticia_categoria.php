a5fab6bcc5abed8680146137d7874e322f4ca1e6
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-17 13:59:48 +0200

    Atualizações

--- site/components/noticia_categoria.php
+++ site/components/noticia_categoria.php
@@ -95,21 +95,36 @@
           while($d = mysqli_fetch_object($result)){
           ?>
 
-          <div style="margin-top:15px" class="col-lg-3" data-aos="fade-up" data-aos-delay="200">
+          <!-- <div style="margin-top:15px" class="col-lg-3" data-aos="fade-up" data-aos-delay="200">
             <div class="post-box">
               <div class="post-img" style="margin-top:10px;height:267px"><img src="<?=$localPainel?>site/volume/noticias/<?=$d->imagem?>" class="img-fluid" alt=""></div>
-              <!-- <div class="meta">
-                <span class="post-date">Tue, December 12</span>
-                <span class="post-author"> / Julia Parker</span>
-              </div> -->
               <h3 class="post-title" style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;"><?=$d->titulo?></h3>
               <p style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;"><?=strip_tags(str_replace('<',' <',str_replace('>','> ',$d->materia)))?></p>
 
               <a href="noticia.php?cod=<?=$d->codigo?>" class="mt-3 mb-3">
                 <button type="button" class="botao botaoroxo">Leia Mais<i class="bi bi-arrow-right"></i></button>
               </a>
+            </div>
+          </div> -->
 
 
+
+          <div class="col-lg-4" data-aos="fade-up" data-aos-delay="200">
+            <div class="post-box">
+              <div class="post-img evento">
+              <div style="margin-top:10px;height:267px" class="post-img"><img src="<?=$localPainel?>site/volume/noticias/<?=$d->imagem?>" class="img-fluid" alt=""></div>
+                <!-- <div style="background-image:url(<?=$localPainel?>site/volume/noticias/<?=$d->imagem?>);" class="imagemEventoFundo"></div>
+                <div style="background-image:url(<?=$localPainel?>site/volume/noticias/<?=$d->imagem?>);" class="imagemEvento"></div> -->
+              </div>
+              <!-- <div class="meta">
+                <span class="post-date">Tue, December 12</span>
+                <span class="post-author"> / Julia Parker</span>
+              </div> -->
+              <h3 class="post-title" style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical;"><?=$d->titulo?></h3>
+              <p style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical;"><?=strip_tags(str_replace('<',' <',str_replace('>','> ',$d->materia)))?></p>
+              <a href="noticia.php?cod=<?=$d->codigo?>" class="mt-3 mb-3">
+                <button type="button" class="botao botaoroxo">Leia Mais<i class="bi bi-arrow-right"></i></button>
+              </a>
             </div>
           </div>
 
