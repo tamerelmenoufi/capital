ac81986dcdc9c515a55ee97002f42cb0b5ba20c4
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-17 16:14:49 +0200

    Atualizações

--- api/index.php
+++ api/index.php
@@ -2,11 +2,6 @@
     include("/capitalinc/connect.php");
     $con = AppConnect('capital');
     include("classes.php");
-?>
-
-<h3>Sistema de Assessoria Financeira</h3>
-
-<?php
 
 $facta = new Facta;
 
