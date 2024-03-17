fb60c07d738e5123d86cde3da23e03ed1b88ec49
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-17 16:19:41 +0200

    Atualizações

--- api/classes.php
+++ api/classes.php
@@ -24,7 +24,7 @@ class Facta {
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTPS_VERSION => CURL_HTTPS_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'GET',
-        CURLOPT_HTTPSHEADER => array("Authorization: Basic OTY3NTM6a2M4emRmZjljdWxoajFjbGpoZWQ=")
+        CURLOPT_HTTPSHEADER => array("Authorization: Basic OTY3NTM6a2M4emRmZjljdWxoajFjbGpoZWQ=", )
         ));
         $response = curl_exec($curl);
         curl_close($curl);
