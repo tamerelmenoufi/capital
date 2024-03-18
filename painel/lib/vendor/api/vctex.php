23f231f6e9ba21b54128a82c62beb86faff5a303
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-18 22:13:33 +0200

    Atualizações

--- painel/lib/vendor/api/vctex.php
+++ painel/lib/vendor/api/vctex.php
@@ -2,7 +2,7 @@
 
 class Vctex {
 
-    public $ambiente = 'homologacao'; //homologacao ou producao
+    public $ambiente = 'producao'; //homologacao ou producao
 
     public function Ambiente($opc){
         if($opc == 'homologacao'){
