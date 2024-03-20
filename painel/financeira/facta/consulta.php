3ed5fb6346dcceade34a2fc623701743bae663e4
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-20 14:28:11 +0200

    Atualizações

--- painel/financeira/facta/consulta.php
+++ painel/financeira/facta/consulta.php
@@ -76,7 +76,7 @@
         $_SESSION['facta_rotulo'] = $_POST['rotulo'];
         $_SESSION['facta_valor'] = $_POST['valor'];
 
-        echo $query = "select *  from consultas_facta where codigo = '{$_POST['consulta']}'";
+        echo $query = "select *  from consultas_facta where codigo = '{$_POST['calculo']}'";
         $result = mysqli_query($con, $query);
         $d = mysqli_fetch_object($result);
 
