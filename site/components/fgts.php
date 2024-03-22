fe1d3be1dc5896ca20f744d12d8bc7c5767559f0
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-22 23:12:16 +0200

    Atualizações

--- site/components/fgts.php
+++ site/components/fgts.php
@@ -22,6 +22,14 @@
             font-size:40px;
             color:green;
         }
+        .legenda{
+            position:absolute;
+            top:30px;
+            font-size:40px;
+            color:green;
+            width:100px;
+            border:solid 1px red;
+        }
     </style>
     <section id="fgts" class="team">
       <div class="container" data-aos="fade-up">
@@ -33,11 +41,14 @@
                 <div class="fluxo">
                     <div class="linha"></div>
                     <i class="fa-solid fa-circle etapas" style="left:calc(0% - 5px)"></i>
-                    <!-- <i class="fa-solid fa-circle etapas" style="left:calc(20% - 35px)"></i> -->
                     <i class="fa-solid fa-circle etapas" style="left:calc(33% - 20px)"></i>
-                    <!-- <i class="fa-solid fa-circle etapas" style="left:calc(60% - 35px)"></i> -->
                     <i class="fa-solid fa-circle etapas" style="left:calc(66% - 20px)"></i>
                     <i class="fa-solid fa-circle etapas" style="left:calc(100% - 35px)"></i>
+
+                    <div class="legenda" style="left:calc(33% - 20px)">Etapa 1</div>
+                    <div class="legenda" style="left:calc(66% - 20px)">Etapa 2</div>
+                    <div class="legenda" style="left:calc(100% - 35px)">Etapa 3</div>
+
                 </div>
             </div>
         </div>
