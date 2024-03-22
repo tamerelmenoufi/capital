5c7d1b5b3db6e4dc119267b62f3465bc2dd4687d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-22 22:42:12 +0200

    Atualizações

--- site/components/fgts.php
+++ site/components/fgts.php
@@ -15,6 +15,11 @@
             left:0;
             top:20px;
         }
+        .etapas{
+            position:absolute;
+            top:20px;
+            font-size:30px;
+        }
     </style>
     <section id="fgts" class="team">
       <div class="container" data-aos="fade-up">
@@ -24,6 +29,7 @@
         <div class="row gy-5">
             <div class="fluxo">
                 <div class="linha"></div>
+                <i class="fa-solid fa-circle etapas" style="left:0%"></i>
             </div>
         </div>
       </div>
