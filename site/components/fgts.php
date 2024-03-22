f909be7ee4d64bd7c979d07daf8bde218ec7c97a
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-22 22:29:06 +0200

    Atualizações

---
+++ site/components/fgts.php
@@ -0,0 +1,14 @@
+    <!-- ======= FGTS Section ======= -->
+    <style>
+
+    </style>
+    <section id="fgts" class="team">
+      <div class="container" data-aos="fade-up">
+        <div class="section-header">
+          <h2>Antecipação de FGTS</h2>
+        </div>
+        <div class="row gy-5">
+
+        </div>
+      </div>
+    </section><!-- End Team Section -->
