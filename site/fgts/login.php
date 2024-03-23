993a3304d14b7600c20af9fe29f62f6e70d3abcc
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-23 17:27:02 +0200

    Atualizações

--- site/fgts/login.php
+++ site/fgts/login.php
@@ -27,11 +27,40 @@
         
         <div class="input-group flex-nowrap">
             <span class="input-group-text" id="addon-wrapping"><i class="fa-solid fa-mobile-screen-button"></i></span>
-            <input type="text" class="form-control" inputmode="numeric" placeholder="Digite seu telefone" aria-label="Telefone" aria-describedby="addon-wrapping" >
-            <button class="btn btn-outline-secondary" type="button" id="button-addon1">Enviar</button>
+            <input type="text" id="telefone" class="form-control" inputmode="numeric" placeholder="Digite seu telefone" aria-label="Telefone" aria-describedby="addon-wrapping" >
+            <button class="btn btn-outline-secondary enviar" type="button" id="button-addon1">Enviar</button>
         </div>
         <p style="color:#534ab3; font-size:12px">Enviaremos um SMS ou WhatsApp com código de confirmação do seu do seu acesso.</p>
 
     </div>
     </div>
 </div>
+
+<script>
+    $(function(){
+        $("#telefone").mask("(99) 99999-9999");
+
+        $("#enviar").click(function(){
+            telefone = $("#telefone").val();
+            if(!telefone){
+                $.alert({
+                    type:"red",
+                    title:"Erro der Identificação",
+                    content: 'Favor Digite o número de seu telefone!'
+                })
+
+                return false;
+            }
+            if($("#telefone").lenght != 15){
+                $.alert({
+                    type:"red",
+                    title:"Erro der Identificação",
+                    content: 'O telefone informado não está correto!'
+                })
+
+                return false;                
+            }
+        })
+
+    })
+</script>
