4b558e23ce0d665d9a81234f1dc91da6127d1239
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-23 16:38:10 +0200

    Atualizações

--- site/components/fgts.php
+++ site/components/fgts.php
@@ -35,7 +35,7 @@
     <section id="fgts" class="team">
       <div class="container" data-aos="fade-up">
         <div class="section-header">
-          <h2>Antecipação de FGTS</h2>
+          <h4>Antecipação de FGTS</h4>
         </div>
         <pspan style="font-size:14px; color:#a1a1a1; font-weight:bold; margin-left:10px;">Etapas para solicitação</span>
         <div class="row gy-5">
