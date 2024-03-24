9bd63a8eb5596dd74a7f056774ecc58475122889
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-25 00:24:15 +0200

    Atualizações

--- site/components/fgts.php
+++ site/components/fgts.php
@@ -75,11 +75,16 @@
             if($_GET['c']){
             ?>
             localStorage.setItem("codUsr", <?=$_GET['c']?>);
+            codUsr = localStorage.getItem("codUsr");
+            <?php
+            }else{
+            ?>
+            codUsr = localStorage.getItem("codUsr");
             <?php
             }
             ?>
 
-            codUsr = localStorage.getItem("codUsr");
+            
 
             if(codUsr){
                 $.ajax({
