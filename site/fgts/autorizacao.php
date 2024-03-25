77ab8e2ea04e766fbfcd2618348976efa35dba68
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-25 06:27:11 +0200

    Atualizações

--- site/fgts/autorizacao.php
+++ site/fgts/autorizacao.php
@@ -2,6 +2,8 @@
 
 include("{$_SERVER['DOCUMENT_ROOT']}/painel/lib/includes.php");
 
+mysqli_query($con, "update cliente set pre_cadastro = NOW() where pre_cadastro = 0 and codigo = '{$_SESSION['codUsr']}'");
+
 if($_POST['acao'] == 'salvar'){
 
     if($_POST['campo'] == 'cpf'){
