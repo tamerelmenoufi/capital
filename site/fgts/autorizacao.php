ee0fe024565f07fdd5a2ee8bfaaa122515a70b07
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-25 20:18:59 +0200

    Atualizações

--- site/fgts/autorizacao.php
+++ site/fgts/autorizacao.php
@@ -69,6 +69,7 @@ $d = mysqli_fetch_object($result);
         <p class="card-text">Antes de fazer a suas simulações e contratação de antecipação do FGTS, é necessário que autorize os bancos parceiros que possibilitam o acessos do seu saldo na CAIXA.</p>
         <p class="card-text">Preparamos um passo a passo pra você seguir e realizar a autorização de forma simples e prática.</p>
         
+        <p class="card-text">Baixe e instale o aplicativo FGTS.</p>
         <img src="fgts/img/passo_a_passo.png?1" class="img-fluid" alt="Passo a Passo">
 
         <p class="card-text">Autorize essas duas instituições:</p>
@@ -76,7 +77,7 @@ $d = mysqli_fetch_object($result);
         <p class="card-text">CDC SOCEDADE DE CREDITO AO MICROEMPREENDEDOR X E A EMPRESA DE PEQUENO PORTE LTDA</p>
         <p class="card-text">Logo em seguida clique em CONFIRMAR</p>
         
-        
+
         <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" <?=(($d->autorizacao_vctex > 0)?'checked disabled':' id="autorizacao"')?>>
             <label class="form-check-label" for="autorizacao">Marque aqui, caso tenha realizado a autorização dos parceiros do banco.</label>
