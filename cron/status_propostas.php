3cf616ff914d1b8e39acd0141e812c1109450d7c
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-27 17:59:34 +0200

    Atualizações

--- cron/status_propostas.php
+++ cron/status_propostas.php
@@ -37,27 +37,29 @@
 
     echo $query = "select *, proposta->>'$.data.proposalId' as proposalId from consultas where proposta->>'$.statusCode' in ('200', '60')";
     $result = mysqli_query($con, $query);
-    while($d = mysqli_fetch_object($result)){
+    if(mysqli_num_rows($result)){
+        while($d = mysqli_fetch_object($result)){
 
-        $consulta = $vctex->Conculta([
-            'token' => $token,
-            'proposalId' => $d->proposalId
-        ]);
-        $retorno = json_decode($consulta);
-        $status_cod = $retorno->proposalStatusId;
-        $status_msg = $retorno->proposalStatusDisplayTitle;
+            $consulta = $vctex->Conculta([
+                'token' => $token,
+                'proposalId' => $d->proposalId
+            ]);
+            $retorno = json_decode($consulta);
+            $status_cod = $retorno->proposalStatusId;
+            $status_msg = $retorno->proposalStatusDisplayTitle;
 
-        consulta_logs([
-            'proposta' => $d->codigo,
-            'consulta' => $consulta,
-            'codUsr' => $d->cliente
-        ]);
+            consulta_logs([
+                'proposta' => $d->codigo,
+                'consulta' => $consulta,
+                'codUsr' => $d->cliente
+            ]);
 
-        $query = "update `consultas` set 
-                                        proposta = JSON_SET(proposta, '$.statusCode', '{$status_cod}'),
-                                        proposta = JSON_SET(proposta, '$.message', '{$status_msg}')
-                        where codigo = '{$d->codigo}'";
+            $query = "update `consultas` set 
+                                            proposta = JSON_SET(proposta, '$.statusCode', '{$status_cod}'),
+                                            proposta = JSON_SET(proposta, '$.message', '{$status_msg}')
+                            where codigo = '{$d->codigo}'";
 
-        $result = mysqli_query($con, $query);
+            $result = mysqli_query($con, $query);
 
+        }
     }
