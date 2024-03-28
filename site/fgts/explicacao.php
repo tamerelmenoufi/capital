4b03507cad292da432adfddf4a6a9c40c584dd9a
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-28 23:31:31 +0200

    Atualizações

--- site/fgts/explicacao.php
+++ site/fgts/explicacao.php
@@ -31,13 +31,17 @@ include("{$_SERVER['DOCUMENT_ROOT']}/painel/lib/includes.php");
         <h5 class="card-title">Assista o vídeo que preparamos para você</h5>
         <p class="card-text">Somos especialistas em antecipação do FGTS e vamos lhe orientar como obter o benefício. Assista com atenção o vídeo e conheça nossa empresa antes de prosseguir.</p>
         
-        <div class="mt-2">
-            <div class="ratio ratio-16x9">
-                <video width="100%" height="100%" controls>
-                    <source src="fgts/videos/autorizacao.mp4" type="video/mp4">
-                    <!-- <source src="movie.ogg" type="video/ogg"> -->
-                    Your browser does not support the video tag.
-                </video>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="mt-2">
+                    <div class="ratio ratio-16x9">
+                        <video width="100%" height="100%" controls>
+                            <source src="fgts/videos/autorizacao.mp4" type="video/mp4">
+                            <!-- <source src="movie.ogg" type="video/ogg"> -->
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                </div>
             </div>
         </div>
 
