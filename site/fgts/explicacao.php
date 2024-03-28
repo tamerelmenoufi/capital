bf5faa3f171ff016415e3280e886977098ad42f8
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-28 22:44:42 +0200

    Atualizações

--- site/fgts/explicacao.php
+++ site/fgts/explicacao.php
@@ -34,7 +34,7 @@ include("{$_SERVER['DOCUMENT_ROOT']}/painel/lib/includes.php");
         <div class="mt-2">
             <div class="ratio ratio-16x9">
                 <video width="100%" height="100%" controls>
-                    <source src="fgts/videos/autorizacao.mp4" type="video/mov">
+                    <source src="fgts/videos/autorizacao.mp4" type="video/mp4">
                     <!-- <source src="movie.ogg" type="video/ogg"> -->
                     Your browser does not support the video tag.
                 </video>
