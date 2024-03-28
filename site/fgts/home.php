9c0094f659d544eaa4e44d7c0716a678761d8718
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-28 16:24:00 +0200

    Atualizações

--- site/fgts/home.php
+++ site/fgts/home.php
@@ -104,64 +104,7 @@ $d = mysqli_fetch_object($result);
         $("#cpf").mask("999.999.999-99");
 
         <?php
-        //verificação do pré-cadastro
-        if($d->pre_cadastro > 0){
-            ?>
-                $(`i[etapa="fgts/home.php"], div[etapa="fgts/home.php"]`).attr("acao", "lib");
-                $(`i[etapa="fgts/autorizacao.php"], div[etapa="fgts/autorizacao.php"]`).attr("acao", "lib");
-                $(".linha").css("width","33%");
-                $(`i[etapa="fgts/autorizacao.php"]`).removeClass("fa-regular");
-                $(`i[etapa="fgts/autorizacao.php"]`).addClass("fa-solid");
-                $(`i[etapa="fgts/home.php"]`).removeClass("fa-regular");
-                $(`i[etapa="fgts/home.php"]`).addClass("fa-solid");
-            <?php
-            }else{
-            ?>
-                $(`i[etapa="fgts/home.php"], div[etapa="fgts/home.php"]`).attr("acao", "blq");
-                $(`i[etapa="fgts/autorizacao.php"], div[etapa="fgts/autorizacao.php"]`).attr("acao", "blq");
-                $(".linha").css("width","0%");
-                $(`i[etapa="fgts/autorizacao.php"]`).removeClass("fa-solid");
-                $(`i[etapa="fgts/autorizacao.php"]`).addClass("fa-regular");
-                $(`i[etapa="fgts/home.php"]`).removeClass("fa-solid");
-                $(`i[etapa="fgts/home.php"]`).addClass("fa-regular");
-            <?php
-            }
-    
-            //verificação da autorização
-            if($d->autorizacao_vctex > 0){
-            ?>
-                $(`i[etapa="fgts/cadastro.php"], div[etapa="fgts/cadastro.php"]`).attr("acao", "lib");
-                $(".linha").css("width","66%");
-                $(`i[etapa="fgts/cadastro.php"]`).removeClass("fa-regular");
-                $(`i[etapa="fgts/cadastro.php"]`).addClass("fa-solid");
-            <?php
-            }else{
-            ?>
-                $(`i[etapa="fgts/cadastro.php"], div[etapa="fgts/cadastro.php"]`).attr("acao", "blq");
-                $(".linha").css("width","33%");
-                $(`i[etapa="fgts/cadastro.php"]`).removeClass("fa-solid");
-                $(`i[etapa="fgts/cadastro.php"]`).addClass("fa-regular");
-            <?php
-            }
-    
-            //verificação da autorização
-            if($d->cadastro_percentual == 100){
-            ?>
-                $(`i[etapa="fgts/consulta.php"], div[etapa="fgts/consulta.php"]`).attr("acao", "lib");
-                $(".linha").css("width","100%");
-                $(`i[etapa="fgts/consulta.php"]`).removeClass("fa-regular");
-                $(`i[etapa="fgts/consulta.php"]`).addClass("fa-solid");
-            <?php
-            }else{
-            ?>
-                $(`i[etapa="fgts/consulta.php"], div[etapa="fgts/consulta.php"]`).attr("acao", "blq");
-                $(".linha").css("width","66%");
-                $(`i[etapa="fgts/consulta.php"]`).removeClass("fa-solid");
-                $(`i[etapa="fgts/consulta.php"]`).addClass("fa-regular");
-            <?php
-            }
-            
-
+        include("barra_status.php");
 
         if($_SESSION['codUsr']){
         ?>
