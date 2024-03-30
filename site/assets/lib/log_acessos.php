282535c5fae84828cb258b463ce5b8ba4f4127a5
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-30 06:08:50 +0200

    Atualizações

--- site/assets/lib/log_acessos.php
+++ site/assets/lib/log_acessos.php
@@ -1,7 +1,8 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/site/assets/lib/includes.php");
 
-    $dados = str_replace('\n',' ',json_encode($_SERVER));
+    // $dados = str_replace('\n',' ',json_encode($_SERVER));
+    $dados = json_encode($_SERVER);
 
     echo $query = "insert into log_acessos set data = NOW(), dados = '{$dados}'";
     mysqli_query($con, $query);
