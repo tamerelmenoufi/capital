5747234e23c5ea9bcc8b613b3fbd956e10c0f68b
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-30 04:40:55 +0200

    Atualizações

--- site/assets/lib/includes.php
+++ site/assets/lib/includes.php
@@ -3,6 +3,7 @@
     include("/capitalinc/connect.php");
     $con = AppConnect('capital');
     include("classes.php");
+    include("{$_SERVER['DOCUMENT_ROOT']}/painel/lib/fn.php");
 
     // $localPainel = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/painel/";
     // $localSite = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/site/";
