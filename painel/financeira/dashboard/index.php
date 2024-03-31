6c32056199881136e5552d14416355f301014b7c
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-03-31 07:00:43 +0200

    Atualizações

--- painel/financeira/dashboard/index.php
+++ painel/financeira/dashboard/index.php
@@ -81,7 +81,7 @@
                             $blank_spaces--;
                         } elseif ($day_counter <= $days_in_month) {
                             $tem = count($dados["2024-03-".str_pad($day_counter, 2, "0", STR_PAD_LEFT)]);
-                            echo "<td>{$day_counter}".(($tem)?"<div class='registros'><i class="fa-solid fa-user-pen"></i> ".$tem."</div></td>":false);
+                            echo "<td>{$day_counter}".(($tem)?"<div class='registros'><i class=\"fa-solid fa-user-pen\"></i> ".$tem."</div></td>":false);
                             $day_counter++;
                         }
                     }
