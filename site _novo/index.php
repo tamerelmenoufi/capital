318002dce629c0062a9b0b23de2f4ae86e1fc516
tamerelmenoufi <tamer@mohatron.com.br>
2024-04-05 11:16:49 -0400

    Atualizações

--- site _novo/index.php
+++ site _novo/index.php
@@ -2,7 +2,6 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/site/assets/lib/includes.php");
 ?><!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
