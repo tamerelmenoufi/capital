a93bd3c9e7afb526c127a117c25e841ecfebf573
livia128924 <livia128924@gmail.com>
2024-04-05 15:22:50 -0300

    atualizando

--- site_novo/components/menu.php
+++ site_novo/components/menu.php
@@ -57,7 +57,7 @@
 
       <a href="index.php" class=" d-flex align-items-center scrollto me-auto me-lg-0">
         <!-- Uncomment the line below if you also wish to use an image logo -->
-        <img src="assets/img/logonovacp.png" style="height:55px !important;" alt="">
+        <img src="assets/img/logonovacp.png" style="height:75px !important;" alt="">
        
       </a>
 
