6402f2dad1489614fdda15cc468ffc34078971ce
livia128924 <livia128924@gmail.com>
2024-04-05 17:22:17 -0300

    atualizando

--- site_novo/components/apresentacao.php
+++ site_novo/components/apresentacao.php
@@ -16,9 +16,10 @@
     <p style="font-size:35px;font-weight:bold;text-align:center"> Titulo </p>
 <p> é simplesmente um texto fictício da indústria de impressão e composição tipográfica. Lorem Ipsum tem sido o texto fictício padrão da indústria desde 1500, quando um impressor desconhecido pegou uma prova de tipos e a misturou para fazer um livro de espécimes de tipos. Ela sobreviveu não apenas a cinco séculos, mas também ao salto para a composição tipográfica eletrônica, permanecendo essencialmente inalterada. Foi popularizado na década de 1960 com o lançamento de folhas Letraset contendo passagens de Lorem Ipsum e, mais recentemente,
    com software de editoração eletrônica como Aldus PageMaker, incluindo versões de Lorem Ipsum.</p>
-
+   
+   <div style="padding:30px"> </div>
    <center> 
-    <d style="margin-top:10px;padding:10px;background:#fcce00;color:144397;font-size:22px;text-align:center;font-weight:bold">
+    <d style="padding:10px;background:#fcce00;color:144397;font-size:22px;text-align:center;font-weight:bold">
     Faça sua simulação</d>
 </center>
 </div>
