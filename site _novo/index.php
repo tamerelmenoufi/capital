36705b97fa82a67076c26deb7196651dda42b6f0
tamerelmenoufi <tamer@mohatron.com.br>
2024-04-05 11:22:08 -0400

    Atualizações

--- site _novo/index.php
+++ site _novo/index.php
@@ -6,10 +6,12 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
+
   <title>capitalsolucoes</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
+  
   <!-- Favicons -->
   <link href="assets/img/icone.png" rel="icon">
 
