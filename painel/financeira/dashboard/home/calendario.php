7129b42acd901995f87f08ddf06dd603b8b32aae
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-06 21:40:10 +0200

    Atualizações

--- painel/financeira/dashboard/home/calendario.php
+++ painel/financeira/dashboard/home/calendario.php
@@ -15,9 +15,9 @@
     $month_name = date("F", $first_day_of_month);
 
     $dados = [];
-    $query = "select a.codigo as cod_cliente, a.nome, a.cpf, a.ultimo_acesso, b.log, b.codigo from consultas_log b left join clientes a on a.codigo = b.cliente where a.ultimo_acesso like '{$year}-".str_pad($month, 2, "0", STR_PAD_LEFT)."%' order by b.codigo asc";
-    $query = "select ultimo_acesso from clientes where ultimo_acesso like '{$year}-".str_pad($month, 2, "0", STR_PAD_LEFT)."%'";
-    $query = "select * from sisLog where data like '{$year}-".str_pad($month, 2, "0", STR_PAD_LEFT)."%' and tabela = 'clientes' and registro->>'$[0]' > 0 group by registro->>'$[0]'";
+    // $query = "select a.codigo as cod_cliente, a.nome, a.cpf, a.ultimo_acesso, b.log, b.codigo from consultas_log b left join clientes a on a.codigo = b.cliente where a.ultimo_acesso like '{$year}-".str_pad($month, 2, "0", STR_PAD_LEFT)."%' order by b.codigo asc";
+    // $query = "select ultimo_acesso from clientes where ultimo_acesso like '{$year}-".str_pad($month, 2, "0", STR_PAD_LEFT)."%'";
+    $query = "select * from sisLog where data like '{$year}-".str_pad($month, 2, "0", STR_PAD_LEFT)."%' and tabela in ('clientes', 'CONSULTAS_LOG') and registro->>'$[0]' > 0 group by registro->>'$[0]'";
 
     $result = mysqli_query($con,$query);
     while($d = mysqli_fetch_object($result)){
