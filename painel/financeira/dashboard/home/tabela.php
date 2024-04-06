bbb5c3a7361adef7f1d073f7641a3fa7a661fd16
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-06 23:38:40 +0200

    Atualizações

--- painel/financeira/dashboard/home/tabela.php
+++ painel/financeira/dashboard/home/tabela.php
@@ -5,11 +5,49 @@
 
     $query = "select
                 (select count(*) from clientes where data_cadastro like '{$data}%') as novos_cadastros,
-                (select count(*) from consultas where data like '{$data}%') as novos_cadastros,
+                (select count(*) from consultas where data like '{$data}%') as simulacoes,
+                (select count(*) from consultas where data like '{$data}%' and dados->>'$.statusCode' = '200') as simulacoes_positiva,
+                (select count(*) from consultas where data like '{$data}%' and dados->>'$.statusCode' != '200') as simulacoes_negativa,
+                (select count(*) from consultas where data like '{$data}%' and proposta is not null) as propostas,
+                (select count(*) from consultas where data like '{$data}%' and proposta is not null and proposta->>'$.statusCode' = '130') as propostas_pagas,
+                (select count(*) from consultas where data like '{$data}%' and proposta is not null and proposta->>'$.statusCode' in ('200', '95', '60', '61')) as propostas_pendentes,
+                (select count(*) from consultas where data like '{$data}%' and proposta is not null and proposta->>'$.statusCode' not in ('200', '130', '95', '60', '61')) as propostas_erro
             ";
+    $result = mysqli_query($con, $query);
+    $d = mysqli_fetch_object($result);
+
+    $dados = [
+        'Novos Cadastros' => $d->novos_cadastros,
+        'Simulações Realizadas' => $d->simulacoes,
+        'Simulações bem sucedidas' => $d->simulacoes_positiva,
+        'Simulações Negadas' => $d->simulacoes_negativa,
+        'Propostas Realizadas' => $d->propostas,
+        'Antecipação Paga' => $d->propostas_pagas,
+        'Propostas com Pendências' => $d->propostas_pendentes,
+        'Propostas Negadas' => $d->propostas_erro
+    ];
+?>
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Descrição</th>
+      <th scope="col">Quanidade</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+    foreach($dados => $item as $valor){
+?>
+    <tr>
+      <td><?=$item?></td>
+      <td><?=$valor?></td>
+    </tr>
+<?php
+    }
 
 ?>
-
+  </tbody>
+</table>
 
 
 <script>
