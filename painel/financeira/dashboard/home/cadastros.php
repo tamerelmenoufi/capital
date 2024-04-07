4e3d20be3a651a70f4da2921c0747e98240b3dbf
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-08 00:11:20 +0200

    Atualizações

--- painel/financeira/dashboard/home/cadastros.php
+++ painel/financeira/dashboard/home/cadastros.php
@@ -8,7 +8,19 @@
     else $dt = "{$m1}/{$Y1}";
 ?>
 <h5>Cadastros no período de <?=$dt?></h5>
-<table class="table table-hover">
+
+<div class="row">
+    <div class="col-md-5">Nome</div>
+    <div class="col-md-2">CPF</div>
+    <div class="col-md-1">Pré-cadastro</div>
+    <div class="col-md-1">Autorização</div>
+    <div class="col-md-1">Simulação</div>
+    <div class="col-md-1">Cadastro</div>
+    <div class="col-md-1">Contrato</div>
+</div>
+
+
+<!-- <table class="table table-hover">
   <thead>
     <tr>
       <th scope="col" class="text-center">Nome</th>
@@ -20,13 +32,23 @@
       <th scope="col" class="text-center">Contrato</th>
     </tr>
   </thead>
-  <tbody>
+  <tbody> -->
 <?php
     $query = "select * from clientes where data_cadastro like '{$data}%'";
     $result = mysqli_query($con, $query);
     while($d = mysqli_fetch_object($result)){
 ?>
-    <tr>
+    <div class="row">
+        <div class="col-md-5"><?=$d->nome?></div>
+        <div class="col-md-2"><?=$d->cpf?></div>
+        <div class="col-md-1"><i class="fa-regular fa-circle"></i></div>
+        <div class="col-md-1"><i class="fa-regular fa-circle"></i></div>
+        <div class="col-md-1"><i class="fa-regular fa-circle"></i></div>
+        <div class="col-md-1"><i class="fa-regular fa-circle"></i></div>
+        <div class="col-md-1"><i class="fa-regular fa-circle"></i></div>
+    </div>
+    
+    <!-- <tr>
       <td class="text-center"><?=$d->nome?></td>
       <td><?=$d->cpf?></td>
       <td class="text-center"><i class="fa-regular fa-circle"></i></td>
@@ -34,13 +56,15 @@
       <td class="text-center"><i class="fa-regular fa-circle"></i></td>
       <td class="text-center"><i class="fa-regular fa-circle"></i></td>
       <td class="text-center"><i class="fa-regular fa-circle"></i></td>
-    </tr>
+    </tr> -->
+
+
 <?php
     }
 
 ?>
-  </tbody>
-</table>
+  <!-- </tbody>
+</table> -->
 
 
 <script>
