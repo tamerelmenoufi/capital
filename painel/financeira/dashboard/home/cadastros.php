002b17474608f8de1ee6778ae57261555ab530e6
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-07 22:46:47 +0200

    Atualizações

--- painel/financeira/dashboard/home/cadastros.php
+++ painel/financeira/dashboard/home/cadastros.php
@@ -26,17 +26,11 @@
       <td class="text-center"><?=$d->nome?></td>
       <td><?=$d->cpf?></td>
       <td class="text-center">
-        <div class="progress-stacked">
-            <div class="progress" role="progressbar" aria-label="Segment one" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100" style="width: 15%">
-                <div class="progress-bar"></div>
-            </div>
-            <div class="progress" role="progressbar" aria-label="Segment two" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%">
-                <div class="progress-bar bg-success"></div>
-            </div>
-            <div class="progress" role="progressbar" aria-label="Segment three" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
-                <div class="progress-bar bg-info"></div>
-            </div>
-        </div>  
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" aria-label="Segment one" style="width: 15%" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
+            <div class="progress-bar bg-success" role="progressbar" aria-label="Segment two" style="width: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+            <div class="progress-bar bg-info" role="progressbar" aria-label="Segment three" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
       </td>
     </tr>
 <?php
