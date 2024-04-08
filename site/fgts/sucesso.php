cd07cd6fb980002c86c0a58a2efbee8b0bec4fa3
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-09 00:47:17 +0200

    Atualizações

--- site/fgts/sucesso.php
+++ site/fgts/sucesso.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php include("../assets/lib/scripts.php"); ?>
 </head>
 <body>
+    <?php include("../assets/lib/scripts_body.php"); ?>
     <h3>Etapa de contratação</h3>
 </body>
 </html>
