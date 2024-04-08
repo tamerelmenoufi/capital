1d811322a5a814567881acc1796f3a141cfbc6cf
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-08 03:25:12 +0200

    Atualizações

--- painel/financeira/dashboard/home/cadastros.php
+++ painel/financeira/dashboard/home/cadastros.php
@@ -45,6 +45,7 @@
 
     $pre_cadastro = $autorizacao = $simulacao = $cadastro = $contrato = 0;
 
+    $pnt = 1;
     while($d = mysqli_fetch_object($result)){
 
 
@@ -91,7 +92,7 @@
 
 ?>
     <div class="row bg-success bg-opacity-10 p-2 border-bottom">
-        <div class="col-md-5"><?=$d->codigo?> <?=(($d->nome)?:'<span class="text-danger">Sem Identificação</span>')?></div>
+        <div class="col-md-5"><?=$pnt?>. <?=(($d->nome)?:'<span class="text-danger">Sem Identificação</span>')?></div>
         <div class="col-md-2"><?=(($d->cpf)?:'<span class="text-danger">000.000.000-00</span>')?></div>
         <div class="col-md-1">
             <div class="d-block d-md-none d-lg-none d-xl-none d-xxl-none"><i class="<?=(($d->pre_cadastro > 0)?"fa-solid fa-circle-check text-success":"fa-regular fa-circle opacity-25")?>"></i> Pré-cadastro</div>
@@ -126,6 +127,7 @@
     </tr> -->
 
 <?php
+        $pnt++;
     }
 
 ?>
