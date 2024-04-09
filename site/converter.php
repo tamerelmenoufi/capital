692f0539cff61ea3456ca2a29bac0b077b1ad3c6
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-09 03:41:44 +0200

    Atualizações

--- site/converter.php
+++ site/converter.php
@@ -1,7 +1,7 @@
 <?php
 
     $lista = file_get_contents("converter.csv");
-    $linhas = explode("\n", $dados);
+    $linhas = explode("\n", $lista);
 
     foreach($linhas as $i => $l){
         echo $l."<hr>";
