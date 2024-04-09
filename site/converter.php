414919e51a2c3f931db8979092aec1c0300542e2
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-09 03:50:00 +0200

    Atualizações

--- site/converter.php
+++ site/converter.php
@@ -3,11 +3,22 @@
     $lista = file_get_contents("converter.csv");
     $linhas = explode("\n", $lista);
 
+    function formatCpf($value)
+    {
+    $CPF_LENGTH = 11;
+    $cnpj_cpf = preg_replace("/\D/", '', $value);
+    
+        if (strlen($cnpj_cpf) === $CPF_LENGTH) {
+            return preg_replace("/(\d{3})(\d{3})(\d{3})(\d{2})/", "\$1.\$2.\$3-\$4", $cnpj_cpf);
+        } 
+    
+    }
+
     foreach($linhas as $i => $l){
         if($i > 0){
             $c = explode("	",$l);
             echo trim($c[0])."<br>".
-            trim($c[1])."<br>".
+            formatCpf(trim($c[1]))."<br>".
             trim($c[2])."<br>".
             "<hr>";
         }
