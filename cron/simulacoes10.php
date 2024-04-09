44aff9d7cef754f412fafca66c8fc4b3c2507f6e
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-09 05:17:39 +0200

    Atualizações

---
+++ cron/simulacoes10.php
@@ -0,0 +1,103 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/painel/lib/includes.php");
+
+    function numero($v){
+        $remove = [" ","/","-",".","(",")"];
+        return str_replace($remove, false, $v);
+    }
+
+    function consulta_logs($dados){
+        global $con;
+        $query = "insert into consultas_log set 
+                                            consulta = '{$dados['proposta']}',
+                                            cliente = '{$dados['codUsr']}',
+                                            data = NOW(),
+                                            sessoes = null,
+                                            log = '{$dados['consulta']}',
+                                            log_unico = '".md5($dados['consulta'].$dados['proposta'])."'";
+
+        $result = sisLog( $query);
+    }
+
+    $vctex = new Vctex;
+
+    $query = "select *, api_vctex_dados->>'$.token.accessToken' as token from configuracoes where codigo = '1'";
+    $result = sisLog( $query);
+    $d = mysqli_fetch_object($result);
+
+    $token = $d->token;
+    $tabela_padrao = $d->api_vctex_tabela_padrao;
+
+    $agora = time();
+
+    if($agora > $d->api_expira){
+        $retorno = $vctex->Token();
+        $dados = json_decode($retorno);
+        if($dados->statusCode == 200){
+            $tabelas = $vctex->Tabelas($dados->token->accessToken);
+            $token = $dados->token->accessToken;
+            sisLog( "update configuracoes set api_vctex_expira = '".($agora + $dados->token->expires)."', api_vctex_dados = '{$retorno}', api_vctex_tabelas = '{$tabelas}' where codigo = '1'");
+        }else{
+            $tabelas = 'error';
+        }
+    }
+
+
+        $codUsr = 103;
+
+
+        $query = "select * from clientes where codigo = '{$codUsr}'";
+        $result = sisLog( $query);
+        $d = mysqli_fetch_object($result);
+
+        //$tabela_padrao = $tabela_padrao;
+        $tabela_escolhida = $tabela_padrao;
+
+        $simulacao = $vctex->Simular([
+            'token' => $token,
+            'cpf' => str_replace(['-',' ','.'],false,trim($d->cpf)),
+            'tabela' => $tabela_padrao
+        ]);
+        
+        $verifica = json_decode($simulacao);
+        // var_dump($verifica);
+        if($verifica->data->isExponentialFeeScheduleAvailable == true and $verifica->statusCode == 200){
+
+            $simulacao = $vctex->Simular([
+                'token' => $token,
+                'cpf' => str_replace(['-',' ','.'],false,trim($d->cpf)),
+                'tabela' => 0
+            ]);
+
+            $tabela_padrao = 0;
+
+        }
+
+
+        $consulta = uniqid();
+
+
+        $query = "insert into consultas set 
+                                            consulta = '{$consulta}',
+                                            operadora = 'VCTEX',
+                                            cliente = '{$codUsr}',
+                                            data = NOW(),
+                                            tabela_escolhida = '{$tabela_escolhida}',
+                                            tabela = '{$tabela_padrao}',
+                                            dados = '{$simulacao}'
+                                            ";
+        mysqli_query($con, $query);
+
+
+        consulta_logs([
+            'proposta' => mysqli_insert_id($con),
+            'consulta' => $simulacao,
+            'sisUsr' => $codUsr
+        ]);
+
+        // exit();
+
+
+
+
+?>
