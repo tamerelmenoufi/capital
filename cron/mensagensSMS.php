a7e99e222dc33eb8207e85c516d681502d781c02
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-09 17:52:27 +0200

    Atualizações

--- cron/mensagensSMS.php
+++ cron/mensagensSMS.php
@@ -20,17 +20,29 @@
 
 
 
-    $query = "select * from clientes where codigo = '103'";
+    $query = "select a.dados as log, a.cliente as cod_cliente, b.* from consultas a left join clientes b on a.cliente = b.codigo where a.data like '2024-04-09%' and a.dados->>'$.statusCode' = '200' order by b.nome asc";
     $result = sisLog( $query);
     $d = mysqli_fetch_object($result);
 
+    $log = json_decode($d->log);
+
+    $nome = explode(" ",trim($d->nome))[0];
+    $valor = number_format($log->data->simulationData->totalReleasedAmount,2,',','.');
+
+    $mensagem = "Capital Soluções Informa: {$nome}, seu FGTS atualizou, já pode antecipar R${$valor}. Acesse capitalsolucoesam.com.br é fácil, Rápido e Seguro.";
+    $caracteres = strlen($mensagem); 
     $msg_list[] = [
         'to' =>  str_replace(['(',')',' ','-'],false,$d->phoneNumber),
         'message' => "Envio de mensagem Capital Soluções",
-        'reference' => "lote-".date("YmdHis")
+        'reference' => "lote-".date("YmdHis"),
+        'caracteres' => $caracteres,
         ];
 
-    $response = sendMultiple($msg_list);
+    print_r($msg_list);
+
+    
+
+    // $response = sendMultiple($msg_list);
 
     // mysqli_query($con, "update clientes set simulacao_10 = '1' where codigo = '{$d->codigo}'");
 
