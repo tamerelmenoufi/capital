0f931d6b3af28ce7360ba633678ab3649b124409
livia128924 <livia128924@gmail.com>
2024-04-10 11:12:43 -0300

    atualizando

--- site_novo/components/depoimentos.php
+++ site_novo/components/depoimentos.php
@@ -94,7 +94,11 @@
           O que <b style="background:#000">dizem</b> <br> nossos clientes
           </h3>
               <div style="margin-top:10px;" class="post-img">
-              <img src="assets/img/audio.png" class="img-fluid" alt=""></div>
+              
+              <audio controls>         
+              <source src="assets/img/audio.mp3" type="audio/mpeg">
+              </audio>
+              
               <p style="color:#fff;font-size:16px;font-weight:bold;text-align:center"> Rafael souza</p>
               <p style="color:#fff;font-size:12px;text-align:center;margin-top:-15px">ANTECIPE O FGTS</p>
               
@@ -110,7 +114,10 @@
           O que <b style="background:#000">dizem</b> <br> nossos clientes
           </h3>
               <div style="margin-top:10px;" class="post-img">
-              <img src="assets/img/audio.png" class="img-fluid" alt=""></div>
+             
+              <audio controls>         
+              <source src="assets/img/audio.mp3" type="audio/mpeg">
+              </audio>
               <p style="color:#fff;font-size:16px;font-weight:bold;text-align:center"> Tamer Mohamed</p>
               <p style="color:#fff;font-size:12px;text-align:center;margin-top:-15px">ANTECIPE O FGTS</p>
               
@@ -127,11 +134,8 @@
               <div style="margin-top:10px;" class="post-img">
               
               
-              <audio controls>
-  
-          
+              <audio controls>         
               <source src="assets/img/audio.mp3" type="audio/mpeg">
-
               </audio>
               <p style="color:#fff;font-size:16px;font-weight:bold;text-align:center"> Ellem Salvador</p>
               <p style="color:#fff;font-size:12px;text-align:center;margin-top:-15px">ANTECIPE O FGTS</p>
