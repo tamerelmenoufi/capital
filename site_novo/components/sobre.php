60e451dff51620cae33cf64bde2276fb87ee04d4
livia128924 <livia128924@gmail.com>
2024-04-10 15:05:44 -0300

    atualizando

--- site_novo/components/sobre.php
+++ site_novo/components/sobre.php
@@ -153,10 +153,7 @@
 
       </div>
 
-      <div class="container" data-aos="fade-up">
-      <div class="row g-4 g-lg-5" data-aos="fade-up" data-aos-delay="200">
-      <div class="col-lg-6">
-
+     
       <div style="margin-top:20px">
           <p style="color:#144397;font-size:25px;text-align:center;font-weight:bold;font-style:italic">Perguntas Frequentes</p>
 
@@ -178,74 +175,72 @@
     Fale conosco</d>
 </center>
 
-          </div>
-
-          <div class="col-lg-6">
-
-
-          <div class="info">
-              <h3>Entre em contato</h3>
-
-
-
-
-              <div class="info-item d-flex">
-                <i class="bi bi-envelope flex-shrink-0"></i>
-                <div>
-                  <h4>E-mail:</h4>
-                  <p><?=$d->email?></p>
-                </div>
-              </div><!-- End Info Item -->
-
-              <div class="info-item d-flex">
-                <i class="bi bi-phone flex-shrink-0"></i>
-                <div>
-                  <h4>Telefone:</h4>
-                  <p><?=$d->telefone?></p>
-                </div>
-              </div><!-- End Info Item -->
-
-            </div>
-
-          </div>
-
-          <div class="col-lg-7">
-            <form class="php-email-form">
-              <div class="row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Nome Completo" required>
-                </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="E-mail" required>
-                </div>
-              </div>
-
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" id="message" placeholder="Sua mensagem" required></textarea>
-              </div>
-              <div class="text-center"><button type="submit" class="botaodiferente" style="
-                background: #ffffff;
-    border: 0;
-    padding: 13px 50px;
-    color: #393286;
-    transition: 0.4s;
-    border-radius: 25px;
-    border-left: 10px #393286 solid;
-    border-right: #393286 10px solid;
-    border-top: #393286 solid 1px;
-    border-bottom: #393286 solid 1px;
-              "
-              >Enviar</button></div>
-            </form>
-          </div><!-- End Contact Form -->
-
-        </div>
-
-          </div>
-</div>
-</div>
 <div style="padding:15px"> </div>
 
 
     </section><!-- End About Section -->
 
+
+    <div class="container">
+
+<div class="row gy-5 gx-lg-5">
+
+  <div class="col-lg-5">
+
+    <div class="info">
+      <h3>Entre em contato</h3>
+
+
+
+
+      <div class="info-item d-flex">
+        <i class="bi bi-envelope flex-shrink-0"></i>
+        <div>
+          <h4>E-mail:</h4>
+          <p><?=$d->email?></p>
+        </div>
+      </div><!-- End Info Item -->
+
+      <div class="info-item d-flex">
+        <i class="bi bi-phone flex-shrink-0"></i>
+        <div>
+          <h4>Telefone:</h4>
+          <p><?=$d->telefone?></p>
+        </div>
+      </div><!-- End Info Item -->
+
+    </div>
+
+  </div>
+
+  <div class="col-lg-7">
+    <form class="php-email-form">
+      <div class="row">
+        <div class="col-md-6 form-group">
+          <input type="text" name="name" class="form-control" id="name" placeholder="Nome Completo" required>
+        </div>
+        <div class="col-md-6 form-group mt-3 mt-md-0">
+          <input type="email" class="form-control" name="email" id="email" placeholder="E-mail" required>
+        </div>
+      </div>
+
+      <div class="form-group mt-3">
+        <textarea class="form-control" name="message" id="message" placeholder="Sua mensagem" required></textarea>
+      </div>
+      <div class="text-center"><button type="submit" class="botaodiferente" style="
+        background: #ffffff;
+border: 0;
+padding: 13px 50px;
+color: #393286;
+transition: 0.4s;
+border-radius: 25px;
+border-left: 10px #393286 solid;
+border-right: #393286 10px solid;
+border-top: #393286 solid 1px;
+border-bottom: #393286 solid 1px;
+      "
+      >Enviar</button></div>
+    </form>
+  </div><!-- End Contact Form -->
+
+</div>
