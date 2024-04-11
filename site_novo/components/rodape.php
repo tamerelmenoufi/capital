28ad793802b7caa9267ee1d6d2005d3b42d1de9e
livia128924 <livia128924@gmail.com>
2024-04-11 12:11:26 -0300

    atualizando

--- site_novo/components/rodape.php
+++ site_novo/components/rodape.php
@@ -72,3 +72,18 @@
 </div>
 
 </footer><!-- End Footer -->
+
+
+<div class="container text-center">
+  <div class="row align-items-center">
+    <div class="col-3">
+      One of three columns
+    </div>
+    <div class="col-3">
+      One of three columns
+    </div>
+    <div class="col-6">
+      One of three columns
+    </div>
+  </div>
+</div>
