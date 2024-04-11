9ad74d63efa6873c325fba522659f9cd8ea65162
livia128924 <livia128924@gmail.com>
2024-04-11 20:28:46 -0300

    atualizando

--- site_novo/components/servicos.php
+++ site_novo/components/servicos.php
@@ -150,8 +150,8 @@
 
       
    <center> 
-    <d style="padding:10px;background:#fcce00;color:#144397;font-size:25px;text-align:center;font-weight:bold;padding-top:5px;padding-right:25px;padding-left:25px">
-    Mais serviços</d>
+    <a href="servico_categoria.php"><d style="padding:10px;background:#fcce00;color:#144397;font-size:25px;text-align:center;font-weight:bold;padding-top:5px;padding-right:25px;padding-left:25px">
+    Mais serviços</d></a>
 </center>
 <div style="padding:10px"> </div>
       </div>
