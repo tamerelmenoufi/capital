9bc6ef094461e80c08097d62655952744aeccb0e
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-11 23:53:55 +0200

    Atualizações

--- painel/financeira/clientes/index.php
+++ painel/financeira/clientes/index.php
@@ -29,7 +29,7 @@
           <?php
           if($_SESSION['ProjectPainel']->codigo == 2){
           ?>
-          <div class="input-group mb-3">
+          <div class="input-group m-3">
             <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="#">Action</a></li>
