d108304a69e1591f8713059e9dcccc4e4b73cf0c
livia128924 <livia128924@gmail.com>
2024-04-11 12:13:27 -0300

    atualizando

--- site_novo/components/rodape.php
+++ site_novo/components/rodape.php
@@ -77,13 +77,40 @@
 <div class="container text-center">
   <div class="row align-items-center">
     <div class="col-3">
-      One of three columns
+    <p style="color:#fff;font-size:18px;text-align:center;font-weight:bold;font-style:italic;margin-bottom:0px">UNIDADE CIDADE NOVA</p>
+<p style="color:#fff">Rua Prof. felix Valois, 61<br> Cidade nova</p>
     </div>
     <div class="col-3">
-      One of three columns
+    <p style="color:#fff;font-size:18px;text-align:center;font-weight:bold;font-style:italic;margin-bottom:0px">UNIDADE MANOA</p>
+<p style="color:#fff">Av. Francisco Queiroz, 02 - <br> Manoa</p>
+
     </div>
     <div class="col-6">
-      One of three columns
+    <?php
+
+$query = "select * from configuracoes where codigo = '1'";
+$result = sisLog( $query);
+$d = mysqli_fetch_object($result);
+
+$midias = json_decode($d->midias_sociais);
+
+$midias_sociais = [
+  'facebook' => 'https://www.facebook.com/',
+  'twitter' => 'https://twitter.com/',
+  'instagram' => 'https://instagram.com/',
+  'youtube' => 'https://www.youtube.com/',
+  'linkedin' => 'https://www.linkedin.com/',
+  'whatsapp' => 'https://api.whatsapp.com/send?phone='
+];
+
+foreach($midias_sociais as $ind => $url){
+  if($midias->$ind){
+?>
+<a href="<?=$url.$midias->$ind?>" target="_black" class="<?=$ind?>"><i class="bi bi-<?=$ind?>"></i></a>
+<?php
+  }
+}
+?>
     </div>
   </div>
 </div>
