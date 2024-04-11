e1d025c9e999cc1998426940d4a829dd395b307c
livia128924 <livia128924@gmail.com>
2024-04-11 10:15:16 -0300

    atualizando

--- site_novo/components/rodape.php
+++ site_novo/components/rodape.php
@@ -22,11 +22,11 @@
 
     <div class="d-flex flex-column align-items-center align-items-lg-start">
       <div class="copyright">
-      <span style="color:#fff;font-size:18px;text-align:center;font-weight:bold;font-style:italic">UNIDADE CIDADE NOVA</span>
-<span style="color:#fff">Rua Prof. felix Valois, 61<br> Cidade nova</span>
+      <span style="color:#fff;font-size:18px;text-align:center;font-weight:bold;font-style:italic">UNIDADE CIDADE NOVA</p>
+<p style="color:#fff">Rua Prof. felix Valois, 61<br> Cidade nova</p>
 
-<span style="color:#fff;font-size:18px;text-align:center;font-weight:bold;font-style:italic">UNIDADE MANOA</span>
-<span style="color:#fff">Av. Francisco Queiroz, 02 -Cidade nova</span>
+<p style="color:#fff;font-size:18px;text-align:center;font-weight:bold;font-style:italic">UNIDADE MANOA</p>
+<p style="color:#fff">Av. Francisco Queiroz, 02 -Cidade nova</p>
 
 
 
