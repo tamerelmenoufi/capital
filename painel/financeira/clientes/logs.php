b01c4fee65e690abbc62b1cbef88be075db3c0f6
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-13 17:30:04 +0200

    Atualizações

---
+++ painel/financeira/clientes/logs.php
@@ -0,0 +1,11 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/painel/lib/includes.php");
+
+    $query = "select * from consultas_log where cliente = '{$_POST['cliente']}' order by data desc";
+    $result = mysqli_query($con, $query);
+    while($d = mysqli_fetch_object($result)){
+        echo $d->sessao;
+        echo "<br><br>";
+        echo $d->log;
+        echo "<hr>";
+    }
