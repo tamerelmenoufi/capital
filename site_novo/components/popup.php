336aa8a0fcb5e61e6a43f233e22177446e92ec88
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-14 03:30:24 +0200

    Atualizações

--- site_novo/components/popup.php
+++ site_novo/components/popup.php
@@ -1,5 +1,5 @@
 <style>
-    .bg-poup{
+    /* .bg-poup{
         background:#000;
     }
    
@@ -12,7 +12,7 @@
   overflow: hidden;
   margin-left: auto;
   margin-right: auto;
-}
+} */
 
 
 </style>
@@ -21,7 +21,7 @@
 
 
     
-<div class="col" style="padding:0px">
+<div class="col-md-12">
     <p><center style="color:#fff; font-size: 25px; font-weight:bold;margin-bottom:5px"> TODOS OS DIREITOS RESERVADOS </center></p>
  
     <p>A Capital Soluções não é uma instituição financeira e não realiza operações de crédito diretamente. A Capital Soluções é uma plataforma digital e presencial que atua como correspondente bancário para facilitar o processo de contratação de empréstimos. Toda avaliação de crédito será realizada conforme a política de crédito da Instituição Financeira escolhida pelo usuário. Antes da contratação de qualquer serviço através de nossos parceiros, você receberá todas as condições e informações relativas ao produto a ser contrato,de forma completa e transparente. As taxas de juros, margem consignável e prazo de pagamento praticados nos empréstimos com consignação em pagamentodos Governos Federais, Estaduais e Municipais, Forças armadas e INSS observam as determinações de cada convênio, bem como a política de créditoda instituição financeira a ser utilizada. 
