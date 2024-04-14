0fd4b643e029e9c29f16c79e58033e254eb7505d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-14 02:34:33 +0200

    Atualizações

--- site_novo/components/apresentacao.php
+++ site_novo/components/apresentacao.php
@@ -12,8 +12,8 @@ $d = mysqli_fetch_object($result);
 <div class="row">
 
 <div class="col-lg-6">
-<div class='glightbox btn-watch-video d-flex align-items-center'> 
-  <a video <?=(($d->video)?"href='{$d->video}'":false)?> style="position:relative; border:1px red solid" >
+<div class='d-flex align-items-center'> 
+  <a video <?=(($d->video)?"href='{$d->video}'":false)?> class='glightbox btn-watch-video' style="position:relative; border:1px red solid" >
     <img class="img-fluid" src="<?=$localPainel?>site/volume/destaques/<?=$d->imagem?>" style="max-height:550px"/>
     <i class="fa-brands fa-youtube" style="position:absolute; left:50%; top:50%; margin-left:-50px; margin-top:-50px; z-index:1; color:#a1a1a1; font-size:100px;"></i>
   </a>
