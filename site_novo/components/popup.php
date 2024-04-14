371ad2c81c3c73e13974f4cea70190f06de0d347
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-14 03:52:35 +0200

    Atualizações

--- site_novo/components/popup.php
+++ site_novo/components/popup.php
@@ -1,5 +1,13 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/painel/lib/includes.php");
+
+    $query = "select * from configuracoes where codigo = '1'";
+    $result = mysqli_query($con, $query);
+    $d = mysqli_fetch_object($result);
+?>
+
 <style>
-    /* .bg-poup{
+    .bg-poup{
         background:#000;
     }
    
@@ -12,21 +20,8 @@
   overflow: hidden;
   margin-left: auto;
   margin-right: auto;
-} */
-
-
-</style>
-
-
-
-
-    
+}
+</style>   
 <div class="col-md-12">
-    <p><center style="font-size: 25px; font-weight:bold;margin-bottom:5px"> TODOS OS DIREITOS RESERVADOS </center></p>
- 
-    <p>A Capital Soluções não é uma instituição financeira e não realiza operações de crédito diretamente. A Capital Soluções é uma plataforma digital e presencial que atua como correspondente bancário para facilitar o processo de contratação de empréstimos. Toda avaliação de crédito será realizada conforme a política de crédito da Instituição Financeira escolhida pelo usuário. Antes da contratação de qualquer serviço através de nossos parceiros, você receberá todas as condições e informações relativas ao produto a ser contrato,de forma completa e transparente. As taxas de juros, margem consignável e prazo de pagamento praticados nos empréstimos com consignação em pagamentodos Governos Federais, Estaduais e Municipais, Forças armadas e INSS observam as determinações de cada convênio, bem como a política de créditoda instituição financeira a ser utilizada. 
-
-Capital Soluções - CNPJ 27.652.302/0001-09 | Endereço: R. Prof. Félix Valois, 61 - 69095-010 - Manaus, Amazonas.</p>
-    
-
+    <?=$d->direitos?>
 </div>
