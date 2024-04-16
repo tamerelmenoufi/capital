68749d513c42fa235c377790bcf80ed4335e9e76
livia128924 <livia128924@gmail.com>
2024-04-16 18:48:20 -0300

    atualizando

--- site/components/perguntas-frequentes.php
+++ site/components/perguntas-frequentes.php
@@ -100,12 +100,12 @@
 
       </div>
 
-      <div class="container">
+      <div class="container" style="margin-top:10px">
       <div class="accordion" id="accordionExample">
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
+      O que preciso para sacar meu FGTS?
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
@@ -114,10 +114,13 @@
       </div>
     </div>
   </div>
+
+
+
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingTwo">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
+      Quais os documentos necessários para fazer a contratação?
       </button>
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
@@ -126,10 +129,12 @@
       </div>
     </div>
   </div>
+
+
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingThree">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
+      Consigo fazer a contratação online?
       </button>
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
