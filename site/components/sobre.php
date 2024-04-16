25920655a2b57dc642ae34dc2536142a4450e31c
livia128924 <livia128924@gmail.com>
2024-04-16 18:10:57 -0300

    atualizando

--- site/components/sobre.php
+++ site/components/sobre.php
@@ -188,7 +188,7 @@
         </p>
 
         <center> 
-    <a href="servico_categoria.php"><d style="padding:10px;background:#fcce00;color:#144397;font-size:25px;text-align:center;font-weight:bold;padding-top:5px;padding-right:25px;padding-left:25px">
+    <a href="servico_categoria.php"><d style="padding:10px;background:#fcce00;color:#144397;font-size:18px;text-align:center;font-weight:bold;padding-top:5px;padding-right:25px;padding-left:25px">
     Veja as respostas das perguntas aqui</d></a>
 </center>
   <div style="padding:10px"> </div>
