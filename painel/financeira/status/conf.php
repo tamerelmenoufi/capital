5a406f5e6784e9b6dac23aae0f14287f5e37ef3d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-17 09:32:06 +0200

    Atualizações

--- painel/financeira/status/conf.php
+++ painel/financeira/status/conf.php
@@ -1,4 +1,25 @@
 <?php
         include("{$_SERVER['DOCUMENT_ROOT']}/painel/lib/includes.php");
 ?>
-Configurações
+<style>
+    .Titulo<?=$md5?>{
+        position:absolute;
+        left:60px;
+        top:8px;
+        z-index:0;
+    }
+</style>
+<h4 class="Titulo<?=$md5?>">Mensagens - Wapp</h4>
+
+
+
+
+<div class="card" style="width: 18rem;">
+  <div class="card-header">
+    Nome da mensagem
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Olá [nome], ficamos muito felizes com o seu castro do nosso sistema, vamos lhe ajudar a fazer o seu saque o mais rápido possível</li>
+  </ul>
+  <a href="#" class="btn btn-primary btn-sm">Go somewhere</a>
+</div>
