78628b023d5456fd92f4568221ef2e6c31121434
tamerelmenoufi <tamer@mohatron.com.br>
2024-04-19 09:07:55 -0400

    Atualizações

--- painel/financeira/clientes/wapp.php
+++ painel/financeira/clientes/wapp.php
@@ -41,5 +41,12 @@
 <h4 class="Titulo<?=$md5?>">Mensagens WhatsApp</h4>
 <div class="topo<?=$md5?>"><i class="fa-regular fa-comment-dots"></i> <?=$c->nome?></div>
 <div class="palco<?=$md5?>"></div>
-<div class="rodape<?=$md5?>"></div>
+<div class="rodape<?=$md5?>">
+    <div class="d-flex justify-content-between align-items-center m-3">
+        <i class="fa-regular fa-face-smile p-3"></i>
+        <input type="text" class="form-control p-3" id="chatMensagem" aria-describedby="chatMensagem">
+        <i class="fa-solid fa-microphone p-3"></i>
+        <i class="fa-regular fa-paper-plane p-3"></i>
+    </div>
+</div>
 
