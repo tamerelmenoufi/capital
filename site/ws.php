6da76049124d764132d1fc31ed3513dffa08bf0d
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-20 21:04:08 +0200

    Atualizações

--- site/ws.php
+++ site/ws.php
@@ -12,7 +12,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.0.0/socket.io.js"></script>
     <script>
-        const socket = io('https://capitalsolucoesam.com.br:3000');
+        const socket = io('http://206.81.10.165:3000');
 
         socket.on('chat message', function(msg){
             const messages = document.getElementById('messages');
