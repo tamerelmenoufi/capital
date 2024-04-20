89c0eb7dec7715862c4cef7073fd725a7df0eb59
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-20 11:48:03 +0200

    Atualizações

--- cron/webhookWapp.php
+++ cron/webhookWapp.php
@@ -1,7 +1,6 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
-//$_POST = json_decode(file_get_contents('php://input'), true);
-$_POST = file_get_contents('php://input');
+    $_POST = file_get_contents('php://input');
 
 file_put_contents("wgw.txt", print_r($_POST, true));
