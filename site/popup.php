19226d853fb2858eea628848c5ac1d79390142db
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-20 12:52:56 +0200

    Atualizações

--- site/popup.php
+++ site/popup.php
@@ -1,8 +1,8 @@
 <div class="text-center">
     <h5>Olá, você recebeu nossa notificação de saldo no seu FGTS?<br><br>Clique no botão abaixo e fale diretamente com nosso atendimento para solicitar sua antecipação.<br><br>Aguardamos seu contato!</h5>
+    <a class="btn btn-primary btn-lg mt-3" href="https://api.whatsapp.com/send?phone=559281490562&text=Quero%20antecipar%20o%20meu%20saldo%20FGTS" target="_blank">
+        Antecipe seu saldo FGTS
+        <!-- <img src="https://painel.capitalsolucoesam.com.br/site/volume/banners/2916601f40e9f1569fde922481c6c14120240409154815.jpg" class="img-fluid" alt=""> -->
+    </a>
 </div>
 
-<a href="https://api.whatsapp.com/send?phone=559281490562&text=Quero%20antecipar%20o%20meu%20saldo%20FGTS" target="_blank">
-    <button class="btn btn-primary btn-lg">Antecipe seu saldo FGTS</button>
-    <!-- <img src="https://painel.capitalsolucoesam.com.br/site/volume/banners/2916601f40e9f1569fde922481c6c14120240409154815.jpg" class="img-fluid" alt=""> -->
-</a>
