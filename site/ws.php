9b3edeb296b0586a9ca746cea537729b140aadb0
tamerelmenoufi <tamer@mohatron.com.br>
2024-04-22 13:50:25 -0400

    Atualizações

--- site/ws.php
+++ site/ws.php
@@ -37,18 +37,18 @@
         let socket = new WebSocket("wss://javascript.info/article/websocket/demo/hello");
 
         socket.onopen = function(e) {
-        alert("[open] Connection established");
-        alert("Sending to server");
+        console.log("[open] Connection established");
+        console.log("Sending to server");
         socket.send("My name is John");
         };
 
         socket.onmessage = function(event) {
-        alert(`[message] Data received from server: ${event.data}`);
+        console.log(`[message] Data received from server: ${event.data}`);
         };
 
         socket.onclose = function(event) {
         if (event.wasClean) {
-            alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+            console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
         } else {
             // e.g. server process killed or network down
             // event.code is usually 1006 in this case
