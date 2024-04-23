f9206985456ae56d0168b125c234578fa2fa4ab3
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-24 01:35:47 +0200

    Atualizações

--- painel/scripts/migrar_clientes.php
+++ painel/scripts/migrar_clientes.php
@@ -10,7 +10,16 @@
         $tem = mysqli_fetch_object(mysqli_query($con, "select * from clientes where codigo = '{$d->codigo}'"));
 
         if(!$tem->codigo){
-            mysqli_query($con, "insert into cliente set nome = '{$d->nome}', cpf = '{$d->cpf}', phoneNumber = '{$d->telefone}', simulacao_10 = '0', origem = 'BIQ'");
+            mysqli_query($con, "insert into clientes set 
+                                                        nome = '{$d->nome}', 
+                                                        data_cadastro = NOW(),
+                                                        ultimo_acesso = NOW(),
+                                                        cpf = '{$d->cpf}', 
+                                                        phoneNumber = '{$d->telefone}', 
+                                                        simulacao_10 = '0', 
+                                                        origem = 'BIQ'
+                                                        
+                                                        ");
         }
         mysqli_query($con, "update banco set migrado = '1' where codigo = '{$d->codigo}'");
 
