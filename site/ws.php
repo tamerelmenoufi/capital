1b5d637f9fc3fd7576fc45ea904ed70d636b0179
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-24 21:10:41 +0200

    Atualizações

--- site/ws.php
+++ site/ws.php
@@ -3,28 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cliente JavaScript</title>
+    <title>Cliente</title>
 </head>
 <body>
-    <h1>Cliente JavaScript</h1>
-    <textarea id="message" rows="4" cols="50"></textarea><br>
-    <button onclick="sendMessage()">Enviar</button>
-    <div id="response"></div>
+    <output></output>
+    <input type="text" />
 
     <script>
-        function sendMessage() {
-            var message = document.getElementById("message").value;
+        const ws = new WebSocket("ws://206.81.10.165:9501");
+        const input = document.querySelector('input');
+        const output = document.querySelector('output');
 
-            // Criando uma requisição AJAX para enviar a mensagem para o servidor
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", "ws://206.81.10.165:9501", true);
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                    document.getElementById("response").innerText = xhr.responseText;
-                }
-            };
-            xhr.send(message);
-        }
+        ws.addEventListener('open', console.log);
+        ws.addEventListener('message', console.log);
+        
+        ws.addEventListener('message', message => {
+            const dados = JSON.parse(message.data);
+            if(dados.type === 'chat'){
+                output.append('Outro: ' + dados.text, document.createElement('br'));
+            }
+        });
+
+
+        ws.addEventListener('keypress', e => {
+            console.log(e)
+            if(e.code === 'Enter'){
+                const valor = input.value;
+                output.append('Eu: ' + dados.text, document.createElement('br'));
+                ws.send(valor);
+                input.value = '';
+            }
+        });
+
+
     </script>
 </body>
 </html>
