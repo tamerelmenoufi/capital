792dce5de821e58decabee8664f179ae776fcc60
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-25 03:03:14 +0200

    Atualizações

--- site/ws.php
+++ site/ws.php
@@ -10,7 +10,7 @@
     <input type="text" />
 
     <script>
-        const ws = new WebSocket("wss://ws.capitalsolucoesam.com.br");
+        const ws = new WebSocket("ws://206.81.10.165:9501");
         const input = document.querySelector('input');
         const output = document.querySelector('output');
 
