3cfb9c4f1b483c12b344f420fef6c4372bc73065
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-28 01:37:47 +0300

    Atualizações

--- painel/financeira/clientes/wapp.php
+++ painel/financeira/clientes/wapp.php
@@ -91,7 +91,7 @@
     <div class="d-flex justify-content-between align-items-center">
         <span>Mensagens WhatsApp</span>
         <div style="position:relative">
-            <span style="position:absolute; background-color:green; border-radius:100%; width:15px; height:15px; right:10px; top:10px;"></span>
+            <span style="position:absolute; background-color:green; border-radius:100%; width:10px; height:10px; right:0px; top:0px;"></span>
             <i class="fa-solid fa-comments"></i>
         </div>
         
