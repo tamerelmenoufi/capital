bdc7106bd382c915a97f5823b7d94cb9f71eea67
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-28 13:36:04 +0300

    Atualizações

--- painel/financeira/clientes/wapp.php
+++ painel/financeira/clientes/wapp.php
@@ -143,6 +143,7 @@
     .oculta{
         display:none!important;
     }
+
 </style>
 <h4 class="Titulo<?=$md5?>">
     <div class="d-flex justify-content-between align-items-center">
@@ -204,7 +205,7 @@
         <div class="mensagem_audio oculta w-100">
             <div class="d-flex justify-content-between align-items-center w-100">
                 <i class="fa-solid fa-microphone p-3 text-danger"></i>
-                <div class="form-control p-3">Gravando ...</div>
+                <div class="form-control p-3">  <audio id="adioPlay" controls></audio> Gravando ...</div>
             </div>
         </div>        
         <div class="microfone" acao="normal">
@@ -253,6 +254,115 @@
                 $(this).attr("acao","normal");
             }
         })
+
+        ///////////////////////////////////////FUNCAO DO AUDIO//////////////////////////////////
+        let audioIN = { audio: true };
+    //  audio is true, for recording
+  
+    // Access the permission for use
+    // the microphone
+    navigator.mediaDevices.getUserMedia(audioIN)
+  
+      // 'then()' method returns a Promise
+      .then(function (mediaStreamObj) {
+  
+        // Connect the media stream to the
+        // first audio element
+        // let audio = document.querySelector('audio'); ///////////////////////////
+        //returns the recorded audio via 'audio' tag
+  
+        // 'srcObject' is a property which 
+        // takes the media object
+        // This is supported in the newer browsers
+
+        // if ("srcObject" in audio) {
+        //   audio.srcObject = mediaStreamObj;
+        // }
+        // else {   // Old version
+        //   audio.src = window.URL
+        //     .createObjectURL(mediaStreamObj);
+        // }
+  
+        // It will play the audio
+        // audio.onloadedmetadata = function (ev) {////////////////////////////
+  
+        //   // Play the audio in the 2nd audio
+        //   // element what is being recorded
+        // //   audio.play();
+        // };/////////////////////////////////////////
+  
+        // // Start record
+        // let start = document.getElementById('btnStart'); /////////////////////////////
+  
+        // // Stop record
+        // let stop = document.getElementById('btnStop'); //////////////////////////////
+  
+        // // 2nd audio tag for play the audio
+        // let playAudio = document.getElementById('adioPlay'); ////////////////////////
+  
+        // This is the main thing to recorde 
+        // the audio 'MediaRecorder' API
+        let mediaRecorder = new MediaRecorder(mediaStreamObj);
+        // Pass the audio stream 
+  
+        // Start event
+        // start.addEventListener('click', function (ev) {//////////////////////////////////////////////
+        //   mediaRecorder.start();
+        //   // console.log(mediaRecorder.state);
+        // })////////////////////////////////
+
+        $('div[acao="normal"]').click(function(){
+            mediaRecorder.start();
+        })
+  
+        // // Stop event
+        // stop.addEventListener('click', function (ev) {//////////////////////////////////////
+        //   mediaRecorder.stop();
+        //   // console.log(mediaRecorder.state);
+        // }); ////////////////////////////////
+
+        $('div[acao="gravando"]').click(function(){
+            mediaRecorder.stop();
+        })
+  
+        // If audio data available then push 
+        // it to the chunk array
+        mediaRecorder.ondataavailable = function (ev) {
+          dataArray.push(ev.data);
+        }
+  
+        // Chunk array to store the audio data 
+        let dataArray = [];
+  
+        // Convert the audio data in to blob 
+        // after stopping the recording
+        mediaRecorder.onstop = function (ev) {
+  
+          // blob of type mp3
+          let audioData = new Blob(dataArray, 
+                    { 'type': 'audio/mp3;' });
+            
+          // After fill up the chunk 
+          // array make it empty
+          dataArray = [];
+  
+          // Creating audio url with reference 
+          // of created blob named 'audioData'
+          let audioSrc = window.URL
+              .createObjectURL(audioData);
+  
+        //   // Pass the audio url to the 2nd video tag
+        //   playAudio.src = audioSrc; /////////////////////
+          $("#audioPlay").attr("src", audioSrc);
+          $("#audioPlay").css("display", "block");
+        }
+      })
+  
+      // If any error occurs then handles the error 
+      .catch(function (err) {
+        console.log(err.name, err.message);
+      });
+        //////////////////////////////////////FUNCAO DO AUDIO//////////////////////////////////
 
 
         $("#chatMensagem").keypress(function(e){
