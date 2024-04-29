b3fef60f0226f3c617f81825d64f1b883accab39
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-29 21:17:46 +0300

    Atualizações

--- cron/wapp_chat.php
+++ cron/wapp_chat.php
@@ -1,7 +1,7 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/painel/lib/includes.php");
 
-    $tempo = date("Y-m-d H:i:s", mktime(date("H"), date("i"), date("s"), date("m"), date("d"), date("Y")));
+    $tempo = date("Y-m-d H:i:s", mktime(date("H"), date("i"), date("s")-1, date("m"), date("d"), date("Y")));
 
     $query = "SELECT a.*, b.codigo as cod_cliente, b.nome FROM wapp_chat a left join clientes b on REPLACE(REPLACE(REPLACE(REPLACE(b.phoneNumber, '(', ''), ')', ''), '-', ''), ' ', '') = a.de where a.data >= '{$tempo}'";
     $result = mysqli_query($con, $query);
