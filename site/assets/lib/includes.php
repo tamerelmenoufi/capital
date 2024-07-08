0c308ad43bdefc1f45b35123afdb3e8ae1031968
tamerelmenoufi <tamer@mohatron.com.br>
2024-07-08 12:50:52 -0400

    Atualizações

--- site/assets/lib/includes.php
+++ site/assets/lib/includes.php
@@ -13,12 +13,14 @@
 
     $origem = explode(".",$_SERVER['HTTP_X_FORWARDED_HOST'])[0];
 
+    $numWapp = '5592981671574';
+
     if($origem == 'tt'){
-        $UrlWhatsApp = "https://api.whatsapp.com/send?phone=5592981931685&text=Ol%C3%A1!%20Estou%20chegando%20pelo%20TikTok%20e%20gostaria%20de%20adiantar%20o%20meu%20FGTS";
+        $UrlWhatsApp = "https://api.whatsapp.com/send?phone={$numWapp}&text=Ol%C3%A1!%20Estou%20chegando%20pelo%20TikTok%20e%20gostaria%20de%20adiantar%20o%20meu%20FGTS";
     }else if($origem === 'fb'){
-        $UrlWhatsApp = "https://api.whatsapp.com/send?phone=5592981931685&text=Ol%C3%A1!%20Estou%20chegando%20pelo%20Facebook%20e%20gostaria%20de%20adiantar%20o%20meu%20FGTS";
+        $UrlWhatsApp = "https://api.whatsapp.com/send?phone={$numWapp}&text=Ol%C3%A1!%20Estou%20chegando%20pelo%20Facebook%20e%20gostaria%20de%20adiantar%20o%20meu%20FGTS";
     }else{
-        $UrlWhatsApp = "https://api.whatsapp.com/send?phone=5592981931685&text=Ol%C3%A1!%20gostaria%20de%20adiantar%20o%20meu%20FGTS";
+        $UrlWhatsApp = "https://api.whatsapp.com/send?phone={$numWapp}&text=Ol%C3%A1!%20gostaria%20de%20adiantar%20o%20meu%20FGTS";
     }
 
     
